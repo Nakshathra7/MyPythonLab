8f9a33ff3fd6a4fe56f2c1e778dda94f0308d4b2
Nakshathra7 <anumano92@gmail.com>
2020-03-26 14:49:18 -0400

    Create shopping.py

---
+++ shopping.py
@@ -0,0 +1,9 @@
+shopeeList = []
+count = int(input("Enter the Shopping List Item Count : "))
+for i in range(1 count):
+    print("Enter Item ", i, ":")
+    item = int(input())
+    shopeeList.append(item)
+print("Shopping Item List is ", shopeeList)
+
+
